0df1d974d398daa2f0d41fb4a0c0b5f9633fc7ab
saintjohn1992 <saintjohnshane@outlook.com>
2022-11-12 18:24:26 +0100

    made card,main component, css for both files, and now setting up the structural frame for the project

---
+++ src/components/card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card-container">
+        <div className='img-container'>
+            <img src={""} alt='' />
+        </div>
+        
+
+        </div>
+  )
+}
+
+export default Card
