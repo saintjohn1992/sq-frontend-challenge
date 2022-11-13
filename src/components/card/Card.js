18aa0723b7d137c4227c0eb9eee4041035c0a7bf
saintjohn1992 <saintjohnshane@outlook.com>
2022-11-13 20:14:52 +0100

    created a fetch hook to fetch the data from the api, and convert it into json data

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./card.css"
 
 const Card = () => {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchLocation = async () => {
+      await fetch("http://localhost:3000/stores")
+      .then((res) => res.json())
+      .then((data) => { 
+        setData(data);
+        console.log(data);
+      });
+
+    };
+    fetchLocation();
+  }, []);
+
+  if(!data.length) return <div>Loading...</div>
+
   return (
     <div className="card-container">
-        <div className='img-container'>
-            <img src={""} alt='' />
+        <div>
+        
         </div>
         
 
